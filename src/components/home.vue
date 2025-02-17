64417a973f7f91c480b1491a9340c8239aa9a154
dim11010 <johnpaulcarlos124@gmail.com>
2025-02-17 09:18:39 +0800

    Display user name

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,18 +1,26 @@
 <template>
 <Header />
-    <h1> Hello User, Welcome to Home Page!</h1>
+    <h1> Hello {{ Fname }} {{ Lname }}, Welcome to Home Page!</h1>
 </template>
 <script>
 import { ref } from "vue";
 import Header from "./header.vue"
 export default {
     name:'home',
+    data(){
+        return{
+            Fname:'',
+            Lname:''
+        }
+    },
     components:{
         Header 
     },
     mounted(){
         //the user will directly access the login page if localStorage has stored data.
         let users = localStorage.getItem('user-info');
+        this.Fname = JSON.parse(users).Fname;
+        this.Lname = JSON.parse(users).Lname;
         if(!users){
             this.$router.push({name:'login'})
         } 
