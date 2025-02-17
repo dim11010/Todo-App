b7281042fd115d8832935bc71feea1395a7d712d
dim11010 <johnpaulcarlos124@gmail.com>
2025-02-17 09:17:04 +0800

    Add update and add routers

--- src/routers.js
+++ src/routers.js
@@ -2,6 +2,8 @@ import home from './components/home.vue'
 import SignUp from "./components/SignUp.vue"
 import { createRouter,createWebHistory } from 'vue-router'
 import login from "./components/login.vue"
+import add from "./components/add.vue"
+import update from "./components/update.vue"
 const routes = [
     {
         name:'home',
@@ -17,6 +19,16 @@ const routes = [
         name:'login',
         component:login,
         path:'/login'
+    },
+    {
+        name:'add',
+        component:add,
+        path:'/add'
+    },
+    {
+        name:'update',
+        component:update,
+        path:'/update'
     }
 ];
 
